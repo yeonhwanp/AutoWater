c3c875c4779a9283db46ecf1481a8cb901c3d498
Matthew Feng <mattfeng@mit.edu>
2019-05-09 10:37:11 -0400

    Remove old files; add main.js and main.css

--- src/server/control.py
+++ src/server/control.py
@@ -3,4 +3,10 @@ import sqlite3
 
 def request_handler(request):
     page = open("__HOME__/finalproject/server/templates/control.html").read()
+    mainjs = open("__HOME__/finalproject/server/static/js/main.js").read()
+    maincss = open("__HOME__/finalproject/server/static/css/main.css").read()
+
+    page = page.replace("/* main.js */", mainjs)
+    page = page.replace("/* main.css */", maincss)
+
     return page
