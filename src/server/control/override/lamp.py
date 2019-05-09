9e560eee55bd510099cec2cea0ad8d535f35c58c
Matthew Feng <mattfeng@mit.edu>
2019-05-09 01:54:40 -0400

    Add override, TODO: integrate into control

---
+++ src/server/control/override/lamp.py
@@ -0,0 +1,27 @@
+import sqlite3
+from datetime import datetime
+
+schedule_db = "__HOME__/schedule.db"
+
+def request_handler(request):
+    if request["method"] != "POST":
+        return "You must send a POST request."
+
+    try:
+        override = request["form"]["override"]
+        if override == "OFF":
+            override = (False, True)
+        elif override == "ON":
+            override = (True, False)
+        else:
+            override = (False, False)
+    except:
+        return "You need to supply the override state."
+
+    conn = sqlite3.connect(schedule_db)
+    c = conn.cursor()
+    c.execute("""INSERT INTO lamp_override VALUES (?, ?, ?)""", (*override, datetime.now()))
+    conn.commit()
+    conn.close()
+
+    return override
